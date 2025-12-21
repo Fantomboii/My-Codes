241b9537058b7bdc2dc40e0b9a612b928d6255dc
John Allen <johnallenoo29@gmail.com>
2025-12-21 23:27:42 +0530

    Create finding day of the week.py

---
+++ finding day of the week.py
@@ -0,0 +1,107 @@
+D=input()
+N=int(input())
+if D=="Monday":
+    if N%7==1:
+        print("Monday")
+    elif N%7==2:
+        print("Tuesday")
+    elif N%7==3:
+        print("Wednesday")
+    elif N%7==4:
+        print("Thursday")
+    elif N%7==5:
+        print("Friday")
+    elif N%7==6:
+        print("Saturday")
+    else:
+        print("Sunday")
+elif D=="Tuesday":
+    if N%7==1:
+        print("Tuesday")
+    elif N%7==2:
+        print("Wednesday")
+    elif N%7==3:
+        print("Thursday")
+    elif N%7==4:
+        print("Friday")
+    elif N%7==5:
+        print("Saturday")
+    elif N%7==6:
+        print("Sunday")
+    else:
+        print("Monday")
+elif D=="Wednesday":
+    if N%7==1:
+        print("Wednesday")
+    elif N%7==2:
+        print("Thursday")
+    elif N%7==3:
+        print("Friday")
+    elif N%7==4:
+        print("Saturday")
+    elif N%7==5:
+        print("Sunday")
+    elif N%7==6:
+        print("Monday")
+    else:
+        print("Tuesday")
+elif D=="Thursday":
+    if N%7==1:
+        print("Thursday")
+    elif N%7==2:
+        print("Friday")
+    elif N%7==3:
+        print("Saturday")
+    elif N%7==4:
+        print("Sunday")
+    elif N%7==5:
+        print("Monday")
+    elif N%7==6:
+        print("Tuesday")
+    else:
+        print("Wednesday")
+elif D=="Friday":
+    if N%7==1:
+        print("Friday")
+    elif N%7==2:
+        print("Saturday")
+    elif N%7==3:
+        print("Sunday")
+    elif N%7==4:
+        print("Monday")
+    elif N%7==5:
+        print("Tuesday")
+    elif N%7==6:
+        print("Wednesday")
+    else:
+        print("Thursday")
+elif D=="Saturday":
+    if N%7==1:
+        print("Saturday")
+    elif N%7==2:
+        print("Sunday")
+    elif N%7==3:
+        print("Monday")
+    elif N%7==4:
+        print("Tuesday")
+    elif N%7==5:
+        print("Wednesday")
+    elif N%7==6:
+        print("Thursday")
+    else:
+        print("Friday")
+else:
+    if N%7==1:
+        print("Sunday")
+    elif N%7==2:
+        print("Monday")
+    elif N%7==3:
+        print("Tuesday")
+    elif N%7==4:
+        print("Wednesday")
+    elif N%7==5:
+        print("Thursday")
+    elif N%7==6:
+        print("Friday")
+    else:
+        print("Saturday")
